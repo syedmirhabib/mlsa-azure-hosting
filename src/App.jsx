4e0a59f38af56453ce02b6d5568439acbcb9bcc6
Syed Mir Habib <mirakik.habib@gmail.com>
2024-05-07 12:54:33 +0000

    Add my "About me" and social accounts

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Syed Mir Habib",
+  title: "Web Developer & Facebook Marketer",
+  email: "syedmirhabib@gmail.com",
+  gitHub: "https://github.com/syedmirhabib/",
+  instagram: "https://www.instagram.com/syedmirhabib/",
+  linkedIn: "https://www.linkedin.com/in/syedmirhabib/",
   medium: "",
-  twitter: "microsoft",
+  twitter: "https://www.twitter.com/syedmirhabib/",
   youTube: "Code",
 };
 
